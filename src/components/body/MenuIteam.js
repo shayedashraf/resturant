f4598defdf1442886b7a166b9a96924ff5762520
shayed <sashraf171202@bscse.uiu.ac.bd>
2021-10-26 17:34:14 +0600

    Dish detail

--- src/components/body/MenuIteam.js
+++ src/components/body/MenuIteam.js
@@ -7,9 +7,9 @@ const MenuIteam = props => {
         <div>
             <Card style={{ margin: "10px" }}>
                 <CardBody>
-                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{ opacity: 0.5 }} />
+                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{ opacity: 0.7 }} />
                     <CardImgOverlay>
-                        <CardTitle>{props.dish.name}</CardTitle>
+                        <CardTitle style={{ cursor: "pointer" }} onclick={props.onDishSelecct}>{props.dish.name}</CardTitle>
                     </CardImgOverlay>
 
                 </CardBody>
