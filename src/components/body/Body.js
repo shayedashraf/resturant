4b05512a107cb4c5ae143e90e0a4402677ba94a2
shayed <sashraf171202@bscse.uiu.ac.bd>
2021-11-20 12:22:49 +0600

    Routing update v6

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,15 +3,15 @@ import Menu from './Menu';
 import Home from './Home';
 import Contact from './Contact';
 import About from './About';
-import { Route, Routes, Redirect } from 'react-router-dom';
+import { Route, Routes, Redirect, Navigate } from 'react-router-dom';
 
 const Body = () => {
     return (
         <Routes>
-            <Route path="/" exact component={Home} />
+            <Route path="/home" element={<Home />} />
             <Route path="/menu" element={<Menu />} />
-            <Route path="/contact" exact component={Contact} />
-            <Route path="/about" exact component={About} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
         </Routes>
     );
 }
