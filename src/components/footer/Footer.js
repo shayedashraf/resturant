3017158f0f017ac738646310b4b73e586ee0482b
shayed <sashraf171202@bscse.uiu.ac.bd>
2021-11-21 12:09:07 +0600

    Footer add

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className="footer">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <p>Shayed Resturant &#169;copyright 2021</p>
+                    </div>
+                </div>
+            </div>
+
 
         </div>
+
+
     );
 }
 
